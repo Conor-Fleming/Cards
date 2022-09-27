217580962a2ee72d8ce88853b5af04c2c3df1a7b
Conor Fleming <conorfleming03@gmail.com>
2022-09-27 12:38:09 -0700

    adding test for shuffle/created deck file

--- deck/deck_test.go
+++ deck/deck_test.go
@@ -1,16 +1,15 @@
 package deck
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
 
 var deck = []Card{
-	{Ace, Heart}, {Two, Heart}, {Three, Heart}, {Four, Heart}, {Five, Heart}, {Six, Heart}, {Seven, Heart}, {Eight, Heart}, {Nine, Heart}, {Ten, Heart}, {Jack, Heart}, {Queen, Heart}, {King, Heart},
+	{Ace, Spade}, {Two, Spade}, {Three, Spade}, {Four, Spade}, {Five, Spade}, {Six, Spade}, {Seven, Spade}, {Eight, Spade}, {Nine, Spade}, {Ten, Spade}, {Jack, Spade}, {Queen, Spade}, {King, Spade},
 	{Ace, Diamond}, {Two, Diamond}, {Three, Diamond}, {Four, Diamond}, {Five, Diamond}, {Six, Diamond}, {Seven, Diamond}, {Eight, Diamond}, {Nine, Diamond}, {Ten, Diamond}, {Jack, Diamond}, {Queen, Diamond}, {King, Diamond},
 	{Ace, Club}, {Two, Club}, {Three, Club}, {Four, Club}, {Five, Club}, {Six, Club}, {Seven, Club}, {Eight, Club}, {Nine, Club}, {Ten, Club}, {Jack, Club}, {Queen, Club}, {King, Club},
-	{Ace, Spade}, {Two, Spade}, {Three, Spade}, {Four, Spade}, {Five, Spade}, {Six, Spade}, {Seven, Spade}, {Eight, Spade}, {Nine, Spade}, {Ten, Spade}, {Jack, Spade}, {Queen, Spade}, {King, Spade},
+	{Ace, Heart}, {Two, Heart}, {Three, Heart}, {Four, Heart}, {Five, Heart}, {Six, Heart}, {Seven, Heart}, {Eight, Heart}, {Nine, Heart}, {Ten, Heart}, {Jack, Heart}, {Queen, Heart}, {King, Heart},
 }
 
 func TestNew(t *testing.T) {
@@ -18,7 +17,16 @@ func TestNew(t *testing.T) {
 	result := New()
 	equalityCheck := reflect.DeepEqual(expected, result)
 	if equalityCheck != true {
-		t.Errorf("got %q, wanted %q", result, expected)
+		t.Errorf("got %q\n, wanted %q", result, expected)
 	}
-	fmt.Println(result)
+	//fmt.Println(result)
+}
+
+func TestShuffle(t *testing.T) {
+	first := New(Shuffle)
+	second := New(Shuffle)
+	isEqual := reflect.DeepEqual(first, second)
+	if isEqual == true {
+		t.Errorf("Error, both versions of the shuffled deck are the same\n %v,\n %v", first, second)
+	}
 }
