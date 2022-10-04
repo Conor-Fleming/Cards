d9fac497db9fce988b97495de1249530e1688b9e
Conor Fleming <conorfleming03@gmail.com>
2022-10-03 20:18:06 -0700

    creating deck and dealing to player 1 and dealer, will need to clean this up but wanted to get it working

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Conor-Fleming/deck/deck"
 )
 
-type Hand struct {
-}
+type Card = deck.Card
 
 type Player struct {
+	Dealer bool
+	Hand   []deck.Card
 }
 
 // version 1
@@ -17,7 +20,20 @@ func main() {
 
 	//deal cards
 	deck := deck.New(deck.ExtraDecks(decks), deck.Shuffle)
+	dealer, player1 := deal(deck)
+	fmt.Println("The dealers hand:\n", dealer[1:])
+	fmt.Println("Player 1's hand:\n", player1)
 
 	//player can hit or stand
 
+}
+
+func deal(deck []Card) ([]Card, []Card) {
+	var p1 []Card
+	var dealer []Card
+	p1 = append(p1, deck[0])
+	dealer = append(dealer, deck[1])
+	p1 = append(p1, deck[2])
+	dealer = append(dealer, deck[3])
+	return dealer, p1
 }
