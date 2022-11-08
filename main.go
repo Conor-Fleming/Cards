a8aabc5176e6d8ca50dc99b74429188d3b67fd9a
Conor Fleming <conorfleming03@gmail.com>
2022-11-08 07:50:11 -0800

    deal function done
    finished the deal function which takes a dynamic number of players, used pointers to fix display issue when using String method as well, should write some tests for deal function

--- main.go
+++ main.go
@@ -26,41 +26,39 @@ func main() {
 	//Decks sets the amount of extra decks to add to the intial deck
 	deck := deck.New(deck.ExtraDecks(2), deck.Shuffle)
 
-	//get input for how many players
+	// get input for how many players
 	// ?
 
-	//call deal function with that amount
+	// call deal function with that amount
 	allPlayers, deck := deal(deck, 5)
-
-	fmt.Println(len(allPlayers))
-
-	//need to find out how to access contents of allPlayers array
-	//might need pointers?
-
 	for i, v := range allPlayers {
 		if i == len(allPlayers)-1 {
 			continue
 		}
 		fmt.Printf("Player %v: %v\n", i+1, v.String())
 	}
-	fmt.Println("Dealer: ", allPlayers[len(allPlayers)-1].String())
+	fmt.Println("Dealer:", allPlayers[len(allPlayers)-1].String())
 }
 
-func deal(deck Deck, players int) ([]Hand, Deck) {
-	//creating players and dealer hands
-	HandArr := make([]Hand, 0)
+func deal(deck Deck, players int) ([]*Hand, Deck) {
+	// creating players and dealer hands
+	HandArr := make([]*Hand, 0)
 
 	for i := 0; i < players; i++ {
 		var player Hand
-		HandArr = append(HandArr, player)
+		HandArr = append(HandArr, &player)
 	}
 	var dealer Hand
-	HandArr = append(HandArr, dealer)
+	HandArr = append(HandArr, &dealer)
 
-	for _, v := range HandArr {
-		card := deck[0]
-		deck = deck[1:]
-		v = append(v, card)
+	// iterate through players and dealer twice and deal a card on each iteration
+	// similar to how a real game would be dealt
+	for i := 0; i < 2; i++ {
+		for _, v := range HandArr {
+			card := deck[0]
+			deck = deck[1:]
+			*v = append(*v, card)
+		}
 	}
 
 	return HandArr, deck
