3ad26f11501c800e54b6fc2bf6b8222565f4801f
Conor Fleming <conorfleming03@gmail.com>
2022-11-07 17:08:34 -0800

    initial commit, re visiting this project

--- main.go
+++ main.go
@@ -8,32 +8,24 @@ import (
 
 type Card = deck.Card
 
-type Player struct {
-	Dealer bool
-	Hand   []deck.Card
+type Hand []deck.Card
+
+type players struct {
+	dealer bool
+	hand   Hand
 }
 
 // version 1
 func main() {
 	//Decks sets the amount of extra decks to add to the intial deck
 	deck := deck.New(deck.ExtraDecks(2), deck.Shuffle)
-	var card Card
-	for i := 0; i < 10; i++ {
-		card, deck = deck[0], deck[1:]
-		fmt.Println(card)
-	}
 
-	//dealer := Player{
-	//	Dealer: true,
-	//	Hand:   d,
-	//}
-	//player1 := Player{
-	//	Dealer: false,
-	//	Hand:   p1,
-	//}
+	fmt.Println(deck[0])
+	deck = deck[1:]
+	fmt.Println(deck[0])
 
-	//fmt.Println("The dealers hand:\n", dealer.Hand[1:])
-	//fmt.Println("Player 1's hand:\n", player1.Hand)
+	//fmt.Println("The dealers hand:\n", dealer[1:])
+	//fmt.Println("Player 1's hand:\n", player)
 
 	//player can hit or stand
 	//var hit string
@@ -46,75 +38,11 @@ func main() {
 
 }
 
+// will always be only 1 player for first version of program
+func deal(players int) []players
+
 func playerHit(deck []Card) Card {
 	card := deck[0]
 	deck = deck[1:]
 	return card
 }
-
-/*
-fmt.Println("-----BlackJack-----")
-	//Decks sets the amount of extra decks to add to the intial deck
-	decks := 2
-
-	quit := false
-	//main game loop
-	for !quit {
-		//read hit or stay from user
-
-		//
-
-		fmt.Println("Press enter to play again or press q to quit")
-		var input string
-		fmt.Scan(&input)
-		if input == "q" {
-			quit = true
-			fmt.Println("Thanks for playing!")
-			break
-		}
-		if input == "\n" {
-			fmt.Println("continue game")
-		}
-		fmt.Println("invalid input")
-	}
-}
-
-func deal(deck []Card) ([]Card, []Card) {
-	var p1 []Card
-	var dealer []Card
-	p1 = append(p1, deck[0])
-	dealer = append(dealer, deck[1])
-	p1 = append(p1, deck[2])
-	dealer = append(dealer, deck[3])
-	return dealer, p1
-}
-
-func gameLoop() {
-	//deal cards
-	deck := deck.New(deck.ExtraDecks(decks), deck.Shuffle)
-	d, p1 := deal(deck)
-	dealer := Player{
-		Dealer: true,
-		Hand:   d,
-	}
-	player1 := Player{
-		Dealer: false,
-		Hand:   p1,
-	}
-
-	fmt.Println("The dealers hand:\n", dealer.Hand[1:])
-	fmt.Println("Player 1's hand:\n", player1.Hand)
-
-	//player can hit or stand
-	fmt.Println("Press 'h' to hit or 's' to stay")
-}
-
-func getHandTotal(hand []Card) int {
-	total := 0
-	for _, v := range hand {
-		total += int(v.Value)
-	}
-	return total
-}
-
-*/
