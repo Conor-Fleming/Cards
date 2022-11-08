b3daf4ec533e8081a6c9d174426c39f3f9dce180
Conor Fleming <conorfleming03@gmail.com>
2022-11-07 21:33:45 -0800

    created dynamic deal function

--- main.go
+++ main.go
@@ -24,23 +24,41 @@ func main() {
 	//Decks sets the amount of extra decks to add to the intial deck
 	deck := deck.New(deck.ExtraDecks(2), deck.Shuffle)
 
-	player, deck := deal(deck)
-	fmt.Println("Player hand: ", player.String())
-	dealer, deck := deal(deck)
-	fmt.Println("Dealer Hand: ", dealer.String())
+	//get input for how many players
+	// ?
 
-}
+	//call deal function with that amount
+	allPlayers := deal(deck, 1)
 
-func deal(deck []deck.Card) (Hand, []deck.Card) {
-	var hand Hand
-	for i := 0; i < 2; i++ {
-		hand = append(hand, deck[i])
-		deck = deck[1:]
+	for i, v := range allPlayers {
+		if i == len(allPlayers)-1 {
+			continue
+		}
+		fmt.Printf("Player %v: %v\n", i+1, v.String())
 	}
-	return hand, deck
+	fmt.Println("Dealer: ", allPlayers[len(allPlayers)-1].String())
 }
 
-func drawCard(deck []deck.Card) (Card, []deck.Card) {
+func deal(deck []deck.Card, players int) []Hand {
+	//creating players and dealer hands
+	HandArr := make([]Hand, players+1)
+	for i := 0; i < players; i++ {
+		var player Hand
+		HandArr = append(HandArr, player)
+	}
+	var dealer Hand
+	HandArr = append(HandArr, dealer)
+
+	for _, v := range HandArr {
+		card := deck[0]
+		deck = deck[1:]
+		v = append(v, card)
+	}
+
+	return HandArr
+}
+
+func drawCard(deck []deck.Card) (Card, Hand) {
 	card := deck[0]
 	deck = deck[1:]
 	return card, deck
