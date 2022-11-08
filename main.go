2ede68ffd8706d1a81302d698a4c563ce0eb0814
Conor Fleming <conorfleming03@gmail.com>
2022-11-07 17:52:33 -0800

    created strings method for Hand type

--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Conor-Fleming/deck/deck"
 )
 
 type Card = deck.Card
 
+type Hand []Card
+
+func (h Hand) String() string {
+	stringsArr := make([]string, len(h))
+	for i := range h {
+		stringsArr[i] = h[i].String()
+	}
+	return strings.Join(stringsArr, ", ")
+}
+
 // version 1
 func main() {
 	//Decks sets the amount of extra decks to add to the intial deck
 	deck := deck.New(deck.ExtraDecks(2), deck.Shuffle)
+
+	player := Hand{}
+
 	test, deck := drawCard(deck)
-	fmt.Println(test)
+	player = append(player, test)
+	test, deck = drawCard(deck)
+	player = append(player, test)
+	test, deck = drawCard(deck)
+	player = append(player, test)
+	fmt.Println(player.String())
 
 }
 
